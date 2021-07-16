58e7d4c03263c9d84164a40e7bdd6b57a15566ea
Sangraam Patwardhan <sangraampatwardhan@Sangraams-MacBook-Pro.local>
2021-07-16 21:30:39 +0530

    Modified components and added router

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const ExpenseDashboardPage = () => <div>Dashboard component</div>
+
+export default ExpenseDashboardPage
---
+++ src/components/HelpPage.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const HelpPage = () => <div>Help component</div>
+
+export default HelpPage
